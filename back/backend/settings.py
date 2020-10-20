f8ddeaedad960fba19196376f5a3ea8441164926
jinyeop <wlsduq1111@naver.com>
2020-10-20 17:13:58 +0900

    S03P31D104-16 | post modeling

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -11,25 +11,27 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
-from settings_json import get_setting
-
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+with open(os.path.join(BASE_DIR,'backend','settings.json'), 'r') as f:
+    temp = ''
+    for s in f:
+        temp += s
+    setting = json.loads(str(temp))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_setting("SECRET_KEY")
+SECRET_KEY = setting["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -42,6 +44,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # rest framework
     "rest_framework",
+
+    # app
+    "accounts",
+    "post",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+AUTH_USER_MODEL = "accounts.User"
