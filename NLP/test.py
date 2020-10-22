ab0f732f3a90bdc7ab535899fc15e76fcab1abf1
JJS <cjs1216@gmail.com>
2020-10-22 23:38:00 +0900

    S03P31D104-23 | 감성사전 중복 제거
    Former-commit-id: 8fce97ee66765e6cf239218a47f0d6b84e3cb868

--- NLP/test.py
+++ NLP/test.py
@@ -1,4 +1,5 @@
 from wordcloud import WordCloud, STOPWORDS
+from eunjeon import Mecab
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -20,31 +21,23 @@ def count_words(text):
             word_counts[word[0]] = word_counts.get(word[0], 0) + 1
     return word_counts
 
-def decompose(ls,score):
+def decompose(ls):
     tag = ['VCP', 'VCN', 'NNG', 'IC', 'MAG', 'VA', 'VV', 'XR']
-
-    if score == 1:
-        tag.pop(1)
-    elif score == -1:
-        tag[1:]
     
     word_list = []
 
-    for txt in ls:
-        out = mecab.pos(txt)
-        for word in out:
-            if word[1] in tag:
-                word_list.append(word[0])
+    for word in ls:
+        if word[1] in tag:
+            word_list.append(word[0])
     
-    return list(set(word_list))
+    return word_list
 
-def day_score(text):
-    
+def day_score(text):    
     cnt = 0
     score = 0
     p = []
     n = []
-    txt = decompose(text,0)
+    txt = decompose(text)
     for word in txt:
         if word in pos:
             score += 1
@@ -59,59 +52,35 @@ def day_score(text):
     return score/cnt
 
 
-from eunjeon import Mecab
 mecab = Mecab()
-text = "희망고문을 주다"
+text = open('text.txt', "r").read()
 # 텍스트를 형태소 단위로 나누는 함수. 파라미터의 norm은 문장을 정규화, stem은 각 단어에서 어간을 추출
-out1 = mecab.morphs(text)
+# out1 = mecab.morphs(text)
 # 텍스트에서 명사만 추출하는 함수
 # out2 = mecab.nouns(text)
 # 각 품사를 태깅하는 함수. 텍스트를 형태소 단위로 나눈 후 나뉘어진 형태소를 해당하는 품사와 함께 리스트화.
 # 파라미터에는 norm, stem, join(나눠진 형태소와 품사를 형태소/품사 형태로 리스트화)
 out3 = mecab.pos(text)
-print('morphs',out1)
+# print('morphs',out1)
 # print('nouns',out2)
-print('pos',out3)
+# print('pos',out3)
 # print()
 
 out4 = count_words(text)
 print('word count', out4)
 
-positive = pd.read_csv('positive.txt', sep="\t", engine="python", encoding = "utf-8")
-pos = decompose(positive.T.values[0],1)
-with open('pos.txt', 'w') as f:
-    f.write(str(pos))
-f.close()
+pos = open('pos.txt', "r").read().split('\n')
+neg = open('neg.txt', "r").read().split('\n')
 
-negative = pd.read_csv('negative.txt', sep="\t", engine="python", encoding = "utf-8")
-neg = decompose(negative.T.values[0],-1)
-with open('neg.txt', 'w') as f:
-    f.write(str(neg))
-f.close()
+pos.pop()
+neg.pop()
 
-unknown = pd.read_csv('unknown.txt', sep="\t", engine="python", encoding = "utf-8")
-unk = decompose(unknown.T.values[0],0)
-with open('unk.txt', 'w') as f:
-    f.write(str(unk))
-f.close()
+print(len(pos))
+# print(neg)
 
-score = day_score(out1)
+score = day_score(out3)
 print(score)
 
-chk = []
-for word in pos:
-    if word in neg:
-        chk.append(word)
-
-for word in neg:
-    if word in pos:
-        chk.append(word)
-
-chk = list(set(chk))
-
-with open('chk.txt', 'w') as f:
-    f.write(str(chk))
-f.close()
 
 # spwords = set(STOPWORDS)
 
