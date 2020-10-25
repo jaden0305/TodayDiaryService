3d6e4b4e07528134b858bc643f8567d645801010
yeonjudkwl <hyj9785@likelion.org>
2020-10-25 23:52:12 +0900

    S03P31D104-19 | modal bug

--- front/src/components/modal/ToastSticker.vue
+++ front/src/components/modal/ToastSticker.vue
@@ -2,12 +2,12 @@
 	<section class="toast" :class="toastAnimationClass">
 		<section class="toast-wrap">
 			<div class="toast-stickers">
-				<router-link :to="{ name: 'stickerAll' }">전체</router-link>
+				<!-- <router-link :to="{ name: 'stickerAll' }">전체</router-link>
 				<router-link :to="{ name: 'stickerFigure' }">도형</router-link>
 				<router-link :to="{ name: 'stickerAnimal' }">동물</router-link>
 				<div class="toast-sticker">
 					<router-view></router-view>
-				</div>
+				</div> -->
 			</div>
 			<div class="toast-close">
 				<button class="toast-close__btn" @click="open = false">
