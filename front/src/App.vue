f0bbb416c97e21bf3bc0f1d60b2bf0fefca8f1f0
yeonjudkwl <hyj9785@likelion.org>
2020-10-28 17:20:01 +0900

    S03P31D104-18 | writeDiaryPage styling
    Former-commit-id: d0ab1d3059fa8051089b8aef0f363e2ff36b7fdd

--- front/src/App.vue
+++ front/src/App.vue
@@ -34,6 +34,11 @@ export default {
 	max-width: 768px;
 	min-width: 280px;
 	margin: 0 auto;
+	box-sizing: border-box;
+	border: 8px solid #f0f0f0;
+	border-radius: 15px;
+	background: #f0f0f0;
+	box-shadow: inset 7px 7px 12px #dfdfdf, inset -7px -7px 12px #ffffff;
 	header {
 		display: flex;
 		justify-content: center;
