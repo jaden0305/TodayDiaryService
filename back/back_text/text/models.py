0a835d2139a8419293995d4ddfe83277b7d443ce
jinyeop <wlsduq1111@naver.com>
2020-10-28 16:55:01 +0900

    S03P31D104-46 | add text_analysis response
    Former-commit-id: e1c1b47499d176db2ec0225f94023f9fd5dc8ef8

--- back/back_text/text/models.py
+++ back/back_text/text/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 
-from post.models import Post
+from post.models import Post, Emotion
 
 
 class WordCloudReport(models.Model):
@@ -12,8 +12,34 @@ class WordCloudReport(models.Model):
     date = models.DateField()
 
 
-class TextReport(models.Model):
+class DailyReport(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='text_reports')
     score = models.FloatField()
     emotions = models.TextField()
+    emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE, null=True)
+    date = models.DateField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+# class WeeklyReport(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='text_reports')
+#     score = models.FloatField()
+#     emotions = models.TextField()
+#     emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE)
+#     date = models.DateField()
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+# class MonthlyReport(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='text_reports')
+#     score = models.FloatField()
+#     emotions = models.TextField()
+#     emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE)
+#     date = models.DateField()
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+# class TotalReport(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='text_reports')
+#     score = models.FloatField()
+#     emotions = models.TextField()
+#     emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE)
+#     date = models.DateField()
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
