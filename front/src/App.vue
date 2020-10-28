c83dcd10cbb8800627bf4c795ae99cf1f24287da
yeonjudkwl <hyj9785@likelion.org>
2020-10-28 12:37:37 +0900

    S03P31D104-45 | logo
    Former-commit-id: bfeee0fa684bc9791f46342c00b6bd8e2981ca65

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<div id="app">
+		<header><img src="@/assets/images/logo3.png" alt="오늘하루로고" /></header>
 		<main class="container">
 			<router-view> </router-view>
 		</main>
@@ -20,8 +21,15 @@ export default {};
 	min-height: 100vh;
 	max-width: 768px;
 	min-width: 280px;
-	/* margin: 0 auto; */
-	/* padding: 10px; */
+	margin: 0 auto;
+	header {
+		display: flex;
+		justify-content: center;
+		padding: 18px 0 25px;
+		img {
+			width: 150px;
+		}
+	}
 }
 .container {
 	flex: 1 1 auto;
