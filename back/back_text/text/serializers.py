310e4a89f3e1b05d74acd323395baae6829fc08d
JongWoo <jongwoo328@gmail.com>
2020-10-30 12:33:55 +0900

    migrate
    Former-commit-id: 0f6c18be18a33032b4544e3cd7b9b72cd70e1b1f

--- back/back_text/text/serializers.py
+++ back/back_text/text/serializers.py
@@ -3,17 +3,17 @@ from rest_framework import serializers
 # from accounts.serializers import UserSerializer
 from .models import *
 
-# class WordCloudReportSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = WordCloudReport
-#         fields = '__all__'
+class WordCloudReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WordCloudReport
+        fields = '__all__'
 
-# class MultipleEmotionSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = DailyReport
-#         fields = ('emotions', 'date')
+class MultipleEmotionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DailyReport
+        fields = ('emotions', 'date')
 
-# class DailyReportSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = DailyReport
-#         fields = ('score', 'emotion', 'date')
+class DailyReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DailyReport
+        fields = ('score', 'emotion', 'date')
