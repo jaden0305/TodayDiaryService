82e151c74b7353ac9abe476011f815d7ba0d2900
JongWoo <jongwoo328@gmail.com>
2020-10-30 22:08:16 +0900

    S03P31D104-12 | update calendar logic
    Former-commit-id: ab9e501d85be16331238e81f5a9eb4d1f3e3a106

--- back/back_post/post_calendar/views.py
+++ back/back_post/post_calendar/views.py
@@ -1,4 +1,10 @@
+import datetime
+
+from dateutil.relativedelta import relativedelta
+
 from rest_framework import status
+from rest_framework import permissions
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -8,16 +14,44 @@ from .common import date_to_dict
 from .serializers import CalendarQuerySerializer
 
 
-@swagger_auto_schema(methods=['get'], query_serializer=CalendarQuerySerializer)
-@api_view(['GET'])
-def get_calendar_info(request):
-    year = request.GET.get('year')
-    month = request.GET.get('month')
+class CalendarView(APIView):
 
-    if year and month:
-        calendar_info = date_to_dict(year, month)
-        return Response(calendar_info, status=status.HTTP_200_OK)
-    message = {
-        'deatail': '년도 또는 월 정보가 누락되었습니다.'
-    }
-    return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    permission_classes = (permissions.IsAuthenticated, )
+
+    @swagger_auto_schema(query_serializer=CalendarQuerySerializer)
+    def get(self, request):
+
+        year = int(request.GET.get('year'))
+        month = int(request.GET.get('month'))
+
+        if year and month:
+            calendar_info = date_to_dict(year, month)
+
+            now = datetime.date(year, month, 1)
+            pre = (now - relativedelta(months=1)).month
+            nex = (now + relativedelta(months=1)).month
+            
+            has_before = calendar_info[pre][0]
+            if  has_before:
+                start_month = pre
+                start_day = has_before[0]['day']
+            else:
+                start_month = now.month
+                start_day = 1
+            
+            has_next = calendar_info[nex][-1]
+            if has_next:
+                end_month = nex
+                end_day = has_next[-1]['day']
+            else:
+                end_month = now.month
+                end_day = has_next[-1]['day']
+
+            print('start', start_month, start_day)
+            print('end', end_month, end_day)
+
+            return Response(calendar_info, status=status.HTTP_200_OK)
+        message = {
+            'deatail': '년도 또는 월 정보가 누락되었습니다.'
+        }
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
