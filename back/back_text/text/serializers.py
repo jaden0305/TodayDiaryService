276e6efb057f2d5db6d99e1a6a9f6124bf8df163
JongWoo <jongwoo328@gmail.com>
2020-11-02 19:12:01 +0900

    S03P31D104-46 | update analyze, post logic
    Former-commit-id: 980f2efefaf02a79e7eaaa60cb65c2741e0fe09d

--- back/back_text/text/serializers.py
+++ back/back_text/text/serializers.py
@@ -2,18 +2,22 @@ from rest_framework import serializers
 
 # from accounts.serializers import UserSerializer
 from .models import *
+from post.serializers import EmotionSerializer
 
 class WordCloudReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = WordCloudReport
         fields = '__all__'
 
+
 class MultipleEmotionSerializer(serializers.ModelSerializer):
     class Meta:
         model = DailyReport
         fields = ('emotions', 'date')
 
+
 class DailyReportSerializer(serializers.ModelSerializer):
+    emotion = EmotionSerializer(read_only=True)
     class Meta:
         model = DailyReport
         fields = ('score', 'emotion', 'date')
