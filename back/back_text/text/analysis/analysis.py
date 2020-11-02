81bb0b7671133e66e6b05addd79d1464b927b4b7
JongWoo <jongwoo328@gmail.com>
2020-11-02 15:37:41 +0900

    S03P31D104-46 | remodeling
    Former-commit-id: 162c2b70a9c99417fe2e96b0aa34bad81a2003bb

--- back/back_text/text/analysis/analysis.py
+++ back/back_text/text/analysis/analysis.py
@@ -41,7 +41,10 @@ class TextAnalysis:
         word_list = []
 
         for word in ls:
-            if word[1] in cls.tag and word[0] not in cls.stopwords:
+            if '+' in word[1]:
+                if word[1].split('+')[0] in cls.tag and word[0] not in cls.stopwords:
+                    word_list.append(word[0])
+            elif word[1] in cls.tag and word[0] not in cls.stopwords:
                 word_list.append(word[0])
         
         return set(word_list)
@@ -256,6 +259,10 @@ class TextAnalysis:
                     feel['angry'] = feel.get('angry',0) + 1
                 elif word in self.get_sad():
                     feel['sad'] = feel.get('sad',0) + 1    
+            elif word in self.get_boring():
+                feel['boring'] = feel.get('boring', 0) + 1
+            elif word in self.get_surprise():
+                feel['surprise'] = feel.get('surprise', 0) + 1 
             elif word in self.get_pos():
                 score += 1
                 cnt += 1
