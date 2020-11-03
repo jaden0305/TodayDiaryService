d28811d6ecdb0f6a360616ad6c4b8bdaa860edba
jinyeop <wlsduq1111@naver.com>
2020-11-03 14:26:04 +0900

    S03P31D104-69 | updated daily_report modeling
    Former-commit-id: b3a370b998b913986af72ea16234edd1721db55b

--- back/back_post/text/models.py
+++ back/back_post/text/models.py
@@ -8,14 +8,20 @@ class WordCloudReport(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     word = models.CharField(max_length=50)
     count = models.IntegerField()
-    emotion = models.IntegerField()
+    emotion = models.IntegerField() # 1, 0, -1
     date = models.DateField()
 
 
 class DailyReport(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='text_reports')
     score = models.FloatField()
-    emotions = models.TextField()
     emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE, null=True)
+    user_emotion = models.ForeignKey(Emotion, on_delete=models.CASCADE, blank=True, null=True, related_name='daily_report')
     date = models.DateField()
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+"""
+happy 2 sad 2  -> score에 양수인지 음수인지보고 
+happy 2 delight 2 -> 
+
+"""
