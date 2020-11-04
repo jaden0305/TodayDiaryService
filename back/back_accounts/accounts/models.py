e0c574035698cc2e6f51f2ca09393a278cbe9ada
JongWoo <jongwoo328@gmail.com>
2020-11-04 23:01:25 +0900

    S03P31D104-55 | combine servers
    Former-commit-id: 394aaded3fabdba0dd3b6bcd1e508ad5e850eee6

--- back/back_accounts/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from uuid import uuid4
-
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    class Meta:
-        db_table = 'accounts_user'
