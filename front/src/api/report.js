35a4b5ad0b835b210a0450d929b664b1f53e85c3
marrywill <kti91@likelion.org>
2020-11-10 14:48:23 +0900

    S03P31D104-63 | updated Data
    Former-commit-id: 165a67e55d8420c4a5acb882ecaa2f64e41cb5d9

--- front/src/api/report.js
+++ front/src/api/report.js
@@ -4,5 +4,10 @@ export function fetchMonthReport({ year, month }) {
 	return report.get(`/monthly/?year=${year}&month=${month}`);
 }
 export function fetchWeekReport(startWeek, endWeek) {
-	return report.get(`/weekly/?start=${startWeek}&end=${endWeek}`);
+	const today = new Date();
+
+	return report.get(
+		`/weekly/?start=${startWeek}&end=${endWeek}&today=${today.getFullYear()}-${today.getMonth() +
+			1}-${today.getDate()}`,
+	);
 }
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,49 +1,72 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '../store/index';
+
 Vue.use(VueRouter);
+
+const requireAuth = (to, from, next) => {
+	if (store.getters.getToken === null) {
+		return next();
+	}
+	next('/calendar');
+};
+const notRequireAuth = (to, from, next) => {
+	if (store.getters.getToken !== null) {
+		return next();
+	}
+	next('/');
+};
 
 const routes = [
 	{
 		path: '',
 		name: 'main',
 		component: () => import('@/views/MainPage.vue'),
+		beforeEnter: requireAuth,
 	},
 	{
 		path: '/calendar',
 		name: 'calendar',
 		component: () => import('@/views/CalendarPage.vue'),
+		beforeEnter: notRequireAuth,
 	},
 	{
 		path: '/diary',
 		name: 'diary',
 		component: () => import('@/views/WriteDiaryPage.vue'),
+		beforeEnter: notRequireAuth,
 	},
 	{
 		path: '/diary/:diaryId',
 		name: 'fetchDiary',
 		props: route => ({ propsDiaryId: Number(route.params.diaryId) }),
 		component: () => import('@/views/ReadDiaryPage.vue'),
+		beforeEnter: notRequireAuth,
 	},
 	{
 		path: '/diary/:diaryId/edit',
 		name: 'editDiary',
 		props: route => ({ diaryId: Number(route.params.diaryId) }),
 		component: () => import('@/views/EditDiaryPage.vue'),
+		beforeEnter: notRequireAuth,
 	},
 	{
 		path: '/saveDiary',
 		name: 'saveDiary',
 		component: () => import('@/views/CompleteDiaryPage.vue'),
+		beforeEnter: notRequireAuth,
 	},
 	{
 		path: '/report',
 		name: 'report',
 		component: () => import('@/views/ReportPage.vue'),
+		beforeEnter: notRequireAuth,
 	},
 	{
 		path: '/music',
 		name: 'music',
 		component: () => import('@/views/MusicPlayerPage.vue'),
+		beforeEnter: notRequireAuth,
 	},
 ];
 
