91c33f4c114f7eacd368364f7f40fe2a81973f88
Taein Kim <kti91@likelion.org>
2020-11-13 02:24:59 +0900

    S03P31D104-87 | fix error
    Former-commit-id: 5b866dc2b6981bfe005446e1d1bf5345788e7ef1

--- front/src/components/modal/ToastSave.vue
+++ front/src/components/modal/ToastSave.vue
@@ -1,19 +1,18 @@
 <template>
-	<section class="toast" v-if="this.diaryData" :class="toastAnimationClass">
+	<section class="toast" v-if="diaryData" :class="toastAnimationClass">
 		<section class="toast-wrap">
 			<div class="save-diary">
 				<p class="save-diary-comment">당신의 오늘 하루는</p>
+				<p class="save-diary-comment">{{ diaryData.user_emotion }}이군요 :)</p>
 				<p class="save-diary-comment">
-					{{ this.diaryData.user_emotion }}이군요 :)
-				</p>
-				<p class="save-diary-comment">
-					{{ this.diaryData.recommend_music.artist }}의
-					{{ this.diaryData.recommend_music.name }}으로 마무리하는 건 어때요?
+					{{ diaryData.recommend_music.artist }}의
+					{{ diaryData.recommend_music.name }}으로 마무리하는 건 어때요?
 				</p>
 				<div class="save-diary-emotion">
 					<img
+						v-if="diaryData.user_emotion"
 						:src="
-							require(`@/assets/images/emotion/${this.diaryData.user_emotion}.png`)
+							require(`@/assets/images/emotion/${diaryData.user_emotion}.png`)
 						"
 						alt="감정상태"
 					/>
@@ -35,7 +34,7 @@
 			<div id="mainMenu" class="mainMenuOverlay floating2">
 				<!-- <div class="navire floating3"></div> -->
 				<div
-					v-for="(value, idx) in emotionList(this.diaryData.user_emotion)"
+					v-for="(value, idx) in emotionList(diaryData.user_emotion)"
 					:key="idx"
 					class="itemMenuBox"
 					:class="emotionDesign[idx]"
@@ -148,6 +147,9 @@ export default {
 			emotion.splice(idx - 1, 1);
 			return emotion;
 		},
+	},
+	created() {
+		console.log(this.diaryData);
 	},
 };
 </script>
