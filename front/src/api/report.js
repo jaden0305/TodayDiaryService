6de01304c8a602b65c16e47541d7f80b88f7c849
Taein Kim <kti91@likelion.org>
2020-11-12 21:52:17 +0900

    S03P31D104-63 | updated API
    Former-commit-id: 2ffe2b6140751750a518561f5bf218f4381c854c

--- front/src/api/report.js
+++ front/src/api/report.js
@@ -4,10 +4,5 @@ export function fetchMonthReport({ year, month }) {
 	return report.get(`/monthly/?year=${year}&month=${month}`);
 }
 export function fetchWeekReport(startWeek, endWeek) {
-	const today = new Date();
-
-	return report.get(
-		`/weekly/?start=${startWeek}&end=${endWeek}&today=${today.getFullYear()}-${today.getMonth() +
-			1}-${today.getDate()}`,
-	);
+	return report.get(`/weekly/?start=${startWeek}&end=${endWeek}`);
 }
