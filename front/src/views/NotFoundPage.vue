f5d95ac4903e52a058ff607f356668bb9abd4222
yeonjudkwl <hyj9785@likelion.org>
2020-11-16 02:41:35 +0900

    S03P31D104-87 | page not found
    Former-commit-id: 93996c909cfb0e92be76d2bebfa6b4f495fce4c9

--- front/src/views/NotFoundPage.vue
+++ front/src/views/NotFoundPage.vue
@@ -1,6 +1,7 @@
 <template>
 	<section class="error-wrap">
-		페이지가 없습니다.
+		<img src="@/assets/images/emotion/2.png" alt="슬픔이" />
+		<p>요청하신 페이지가 없어요:(</p>
 	</section>
 </template>
 
@@ -8,4 +9,14 @@
 export default {};
 </script>
 
-<style></style>
+<style lang="scss">
+.error-wrap {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-top: 50px;
+	img {
+		margin-bottom: 20px;
+	}
+}
+</style>
